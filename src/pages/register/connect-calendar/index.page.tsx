e244b5c443467ce61c11a03a8e285d68a7c4dd83
Lubnnia <lubnnia@gmail.com>
2024-01-02 11:38:01 -0300

    feat: Calendar permission - Access permission calendar

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,10 +1,14 @@
-import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn, useSession } from 'next-auth/react'
+
 import { ArrowRight } from 'phosphor-react'
 
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
+  const session = useSession()
+
   // async function handleRegister({ name, username }: RegisterFormData) {}
 
   return (
@@ -22,11 +26,17 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
         </ConnectItem>
+
+        {/* <Text>{JSON.stringify(session.data)}</Text> */}
 
         <Button type="submit">
           Próximo passo
