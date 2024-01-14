a96bca256f45e4de536cef3d32ab92ca07fb91db
Lubnnia <lubnnia@gmail.com>
2024-01-14 18:04:00 -0300

    feat: Calendar and availability: Selected week day

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,4 +1,4 @@
-import { CalendarStep } from './CalendarStep/index.page'
+import { CalendarStep } from './CalendarStep'
 import { ConfirmStep } from './ConfirmStep'
 
 export default function ScheduleForm() {
