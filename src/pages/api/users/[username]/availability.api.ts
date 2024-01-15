dc2cece3a3d114a25e3dac12243dbee1466f682c
Lubnnia <lubnnia@gmail.com>
2024-01-15 17:46:21 -0300

    feat: Calendar and availability: Create schedule table

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,69 @@
+import { prisma } from '@/src/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  // Buscando o username via parâmetro
+  const username = String(req.query.username)
+
+  // Buscando a data via parâmetro
+  const { date } = req.query
+
+  // Se a data não for informada, então uma mensagem de erro será retornada.
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+
+  // Validação se a data escolhida (referenceDate) já é passado, se for será retornado um
+  // array de disponibilidade vazio, ou seja, não existe disponibilidade nenhuma nesse dia.
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({
+    length: endHour - startHour,
+  }).map((_, i) => {
+    // Como o índice começa em zero então é necessário realizar a soma para o primeiro horário,
+    // já sair correto, por exemplo: startHour + 10 = 10... 10 + 1 = 11h...
+    return startHour + i
+  })
+
+  return res.json({ possibleTimes })
+}
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -8,11 +8,17 @@ import {
   TimePickerList,
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -21,7 +27,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            Quarta-feira <span>10 de janeiro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
